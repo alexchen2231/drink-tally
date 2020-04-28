845121cec9eece00fbf58e3a8749bed179e876e9
alexchen2231 <alex.chen.h@gmail.com>
2020-04-27 22:08:08 -0400

    first commit

--- Drink Tally/FirstViewController.swift
+++ Drink Tally/FirstViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var labelCount: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBAction func tapAction(sender: AnyObject) {
+//        let button : UIButton = sender as! UIButton
+        if let text = self.labelCount.text {
+            let newText : String = increaseCounter(text: text)
+            self.labelCount.text = newText
+        }
+    }
+    
+    @IBAction func resetAction(sender: AnyObject) {
+        labelCount.text = "0"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupScreen()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupScreen() {
+        resetButton.tintColor = .black ;
+        tapButton.tintColor = .systemBlue ;
+        tapButton.backgroundColor = .systemTeal ;
+        resetButton.backgroundColor = .systemRed ;
+    }
+    
+    func increaseCounter(text : String) -> String {
+        var value : Int = Int(text)!
+        value = value + 1
+        return String(value)
     }
 
 
